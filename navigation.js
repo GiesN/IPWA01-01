18e71190c0bf308184612310da2b09cc68301ff0
giesn <nils.gies@basf.com>
2023-10-20 10:51:40 +0200

    Changes 20.10.2023

--- navigation.js
+++ navigation.js
@@ -1,26 +1,63 @@
-function home() {
-  var element = document.getElementById("home");
-  element.classList.add("active");
-  var element2 = document.getElementById("aboutus");
-  element2.classList.remove("active");
-  var element3 = document.getElementById("contact");
-  element2.classList.remove("active");
-}
+// function home() {
+//   var element = document.getElementById("home");
+//   element.classList.add("active");
+//   var element2 = document.getElementById("aboutus");
+//   element2.classList.remove("active");
+//   var element3 = document.getElementById("contact");
+//   element2.classList.remove("active");
+// }
 
-function about() {
-  var element = document.getElementById("aboutus");
-  element.classList.add("active");
-  var element2 = document.getElementById("home");
-  element2.classList.remove("active");
-  var element3 = document.getElementById("contact");
-  element3.classList.remove("active");
-}
+// function about() {
+//   var element = document.getElementById("aboutus");
+//   element.classList.add("active");
+//   var element2 = document.getElementById("home");
+//   element2.classList.remove("active");
+//   var element3 = document.getElementById("contact");
+//   element3.classList.remove("active");
+// }
 
-function contact() {
-  var element = document.getElementById("contact");
-  element.classList.add("active");
-  var element2 = document.getElementById("aboutus");
-  element2.classList.remove("active");
-  var element3 = document.getElementById("contact");
-  element3.classList.remove("active");
-}
+// function contact() {
+//   var element = document.getElementById("contact");
+//   element.classList.add("active");
+//   var element2 = document.getElementById("aboutus");
+//   element2.classList.remove("active");
+//   var element3 = document.getElementById("contact");
+//   element3.classList.remove("active");
+// }
+
+// // Add active class to the current button (highlight it)
+// const menuItems = document.getElementsByClassName("nav-link");
+// const navItems = document.getElementsByClassName("nav-item");
+
+// for (let i = 0; i < menuItems.length; i++) {
+//   if (menuItems[i].href === location.href) {
+//     navItems[i].className += " active";
+//   }
+// }
+
+/* Code for changing active  
+            link on clicking */
+// var btns = $(".nav-link");
+
+// console.log(btns);
+
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+
+//     current[0].className = current[0].className.replace(" active", "");
+
+//     this.className += " active";
+//   });
+// }
+
+// const links = document.querySelector(".nav-link");
+// for (let i = 0; i < links.length; i++) {
+//   links[i].addEventListener("click", () => {
+//     const active = document.querySelector(".active");
+//     if (active) {
+//       active.classList.remove("active");
+//     }
+//     links[i].classList.add("active");
+//   });
+// }
--- searchable.js
+++ searchable.js
@@ -1,21 +1,19 @@
 function filterTable() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("inputTable");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("co2Table");
-    tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("inputTable");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("co2Tablebody");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i]
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
+}
